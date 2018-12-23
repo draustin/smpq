3b6f4812424c3ebf7f9745d3efe0a3cad910e5ca
Dane Austin <dane_austin@fastmail.com.au>
2018-12-22 19:45:35 -0800

    Intro moved to readme.

--- smpq/smpq.py
+++ smpq/smpq.py
@@ -1,15 +1,4 @@
 """ Simple multi-processing with queues
-My first serious attempt at multiprocessing with Python ended in failure, in that
-there was no significant speedup. I used multiprocessing.Pool to parallelize
-the calculation of HHG spectra in benzene over different molecular angles. The
-overhead of setting up the process each time (several seconds, worse on Windows)
-prevented any improvement.
-
-Following
-https://pymotw.com/2/multiprocessing/communication.html
-I wrote this simple system based on multiprocessing.Queue. The processes are
-created once, and tasks passed from the main process to the worker processes using
-the queue.
 
 If tasks are defined in __main__, they cannot use anything from their enclosing 
 scope - even modules. I didn't investigate this as I found that putting them in
